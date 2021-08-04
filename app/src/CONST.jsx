bea515044f416062788771ed8375a503f5352172
Angela Candela <angelacandela@hotmail.it>
2021-08-04 16:29:03 +0200

    Added new services

--- app/src/CONST.jsx
+++ app/src/CONST.jsx
@@ -1,2 +1,2 @@
-export const URL_API_BASE = "http://localhost:8000/api/"
-export const URL_APP_BASE = "http://localhost:3000/"
+export const URL_API_BASE = "https://caddy/"
+export const URL_APP_BASE = "https://caddy/"
