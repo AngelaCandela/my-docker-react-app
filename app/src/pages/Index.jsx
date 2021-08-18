e6c74fa1836ea9d0e9e430fdb32ae9edb3e79a8f
MARTINO Marco <marco.martino@ext.ec.europa.eu>
2021-08-18 09:12:19 +0200

    Catch error on fecth products

--- app/src/pages/Index.jsx
+++ app/src/pages/Index.jsx
@@ -3,18 +3,26 @@ import Card from '../components/Card';
 import { URL_API_BASE } from '../CONST';
 
 const Index = () => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(null);
+    const [hasError, setHasError] = useState(false);
 
     useEffect(() => {
         fetch(URL_API_BASE + 'products?page=1')
             .then(response => response.json())
-            .then(data => setProducts(data['hydra:member']));
+            .then(data => setProducts(data['hydra:member']))
+            .catch(error => {
+                 setHasError(true)
+                 console.error('There was an error:', error);
+             });
     }, []);
 
     return (
         <div className="container my-5">
             <h1>{`Listado de productos ${process.env.REACT_APP_NAME}`}</h1>
-            <Card products={products} setProducts={setProducts} />
+
+            {hasError && <p>I'm sorry, something went wrong loading the products!</p>}
+
+            {products && <Card products={products} setProducts={setProducts} />}
         </div>
     )
 };
