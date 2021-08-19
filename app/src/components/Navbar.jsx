d76dfc4ad94decbf1840559cb0d9d81aff66b9b8
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-19 12:58:19 +0200

    fix navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -5,32 +5,11 @@ import mainMenuLinks from "../data/mainMenuLinks.json";
 const Navbar = () => (
     <nav className="navbar navbar-expand navbar-light bg-light">
         <ul className="navbar-nav mr-auto">
-<<<<<<< HEAD
-            <li className="nav-item">
-                <NavLink exact to="/" className="nav-link" activeClassName="active">Inicio</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/create" className="nav-link" activeClassName="active">Crear</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/table" className="nav-link" activeClassName="active">Table</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/contacts" className="nav-link" activeClassName="active">Contacts</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/appointments" className="nav-link" activeClassName="active">Appointments</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/todos" className="nav-link" activeClassName="active">To-do List</NavLink>
-            </li>
-=======
             {mainMenuLinks.map((link, key) =>
               <li key={key} className="nav-item">
                 <LinkItem link={link}/>
               </li>
             )}
->>>>>>> 83a86afac49474a7eda1b852d179c073f7898bbe
         </ul>
     </nav>
 );
