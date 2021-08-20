4f255642933e1d399d2f13930279ae711cd58f88
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-20 11:49:51 +0200

    Add propTypes for HeaderCol and Format code

--- app/src/components/BooksTable.js
+++ app/src/components/BooksTable.js
@@ -2,23 +2,22 @@ import PropTypes from 'prop-types';
 import Book from './Book';
 
 const BooksTable = ({ books }) => (
-    <table className="table table-striped table-hover mx-auto w-75">
-        <thead className="thead-dark">
-            <tr>
-                { books.header.map((header, index) =>
-                    <HeaderCol key={index} header={header} />
-                )}
-            </tr>
-        </thead>
-        <tbody>
-        { books.data.map((book, index) =>
-            <TableRow key={index} book={book} />
+  <table className="table table-striped table-hover mx-auto w-75">
+    <thead className="thead-dark">
+      <tr>
+        {books.header.map((header, index) =>
+          <HeaderCol key={index} header={header} />
         )}
-        </tbody>
-    </table>
+      </tr>
+    </thead>
+    <tbody>
+      {books.data.map((book, index) =>
+        <TableRow key={index} book={book} />
+      )}
+    </tbody>
+  </table>
 );
 
-// TODO: Add propTypes for HeaderCol
 const HeaderCol = ({ header }) => (
   <th>{header}</th>
 )
@@ -26,17 +25,17 @@ const HeaderCol = ({ header }) => (
 // TODO: Add propTypes for TableRow
 const TableRow = ({ book }) => (
   <tr>
-      <Book
-        title={book.title}
-        author={book.author}
-        sales={book.sales}
-        price={book.price}
-        email={book.email}
-      />
+    <Book
+      title={book.title}
+      author={book.author}
+      sales={book.sales}
+      price={book.price}
+      email={book.email}
+    />
   </tr>
 )
 
-const isEmail = function(propValue, key, componentName, location, propFullName) {
+const isEmail = function (propValue, key, componentName, location, propFullName) {
 
   const prop = (location && propFullName) ? propFullName : key;
   const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
@@ -56,7 +55,11 @@ BooksTable.propTypes = {
       price: PropTypes.number.isRequired,
       email: isEmail
     })
-  )
+  ),
 };
+
+HeaderCol.propTypes = {
+  header: PropTypes.string.isRequired
+}
 
 export default BooksTable;
