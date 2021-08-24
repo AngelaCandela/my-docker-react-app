e7ce7307b0454b9a4a47413a160c735329344d71
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-24 14:09:06 +0200

    Change name to CreateProduct

---
+++ app/src/pages/CreateProduct.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import Header1 from "../components/atom/Header1"
+import Container from "../components/layout/Container"
+import ProductForm from "../components/ProductForm";
+
+const CreateProduct = ({ setProducts }) => (
+    <Container className="my-5">
+        <div className="row justify-content-md-center">
+            <Header1 title="Create" />
+        </div>
+        <ProductForm setProducts={setProducts} />
+    </Container>
+);
+
+CreateProduct.propTypes = {
+    setProducts: PropTypes.fuction.isRequired
+}
+
+export default CreateProduct;
