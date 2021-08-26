47fac5b6a39235c7ce9e723a2fb5e94a915d6d39
Angela Candela <angelacandela@hotmail.it>
2021-08-26 18:39:15 +0200

    created ProductsContext

--- app/src/components/context/context.js
+++ app/src/components/context/context.js
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 
 export const AgendaContext = createContext({});
+export const ProductsContext = createContext({});
 
 export const AgendaProvider = AgendaContext.Provider;
+export const ProductsProvider = ProductsContext.Provider;
