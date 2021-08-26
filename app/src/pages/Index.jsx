22f2cc71d03dda501d1e35280244588b5130fc06
Angela Candela <angelacandela@hotmail.it>
2021-08-26 18:56:57 +0200

    removed unnecessary props from Index page

--- app/src/pages/Index.jsx
+++ app/src/pages/Index.jsx
@@ -2,11 +2,11 @@ import Header1 from '../components/atom/Header1'
 import Container from "../components/layout/Container"
 import Card from '../components/Card';
 
-const Index = ({ products, setProducts }) => (
+const Index = () => (
   <Container className="my-5">
     <Header1
         title={`Listado de productos ${process.env.REACT_APP_NAME}`}/>
-    <Card products={products} setProducts={setProducts} />
+    <Card />
   </Container>
 );
 
