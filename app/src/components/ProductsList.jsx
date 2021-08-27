b21a8525862913fd100c1f829dc4f8eb7b1bf8ae
Angela Candela <angelacandela@hotmail.it>
2021-08-27 11:38:18 +0200

    split Card from ProductsList

--- app/src/components/ProductsList.jsx
+++ app/src/components/ProductsList.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { ProductsContext } from "./context/context";
+import Card from "./Card";
 
 const ProductsList = () => {
 
@@ -15,18 +16,7 @@ const ProductsList = () => {
   return (
     <div className="row">
       {products.map(product => {
-        return (
-          <div key={product.name} className="card mt-5 p-3 col-4">
-            <img alt="" src="" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Name: {product.name}</h5>
-              <p className="card-text">Price: {product.price}€</p>
-              <p className="card-text">Id: {product.id}</p>
-              <button onClick={() => history.push('edit/' + product.id)} className="btn btn-primary ">Edit</button>
-              <button onClick={() => remove(product)} className="btn btn-danger deleted ml-3">Delete</button>
-            </div>
-          </div>
-        );
+        return <Card product={product}/>
       })}
     </div>
   )
