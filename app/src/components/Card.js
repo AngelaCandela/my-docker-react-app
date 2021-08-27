d9fc573ab4eba8a21dc9751aebce97149bbf6987
Angela Candela <angelacandela@hotmail.it>
2021-08-27 11:29:06 +0200

    created Card.js

---
+++ app/src/components/Card.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { ProductsContext } from "./context/context";
+
+const Card = ({ product }) => {
+
+  let history = useHistory();
+  const { products, setProducts } = useContext(ProductsContext);
+
+  const remove = (product) => {
+    const filteredProducts = products.filter(item => item.id !== product.id);
+    setProducts(filteredProducts);
+  };
+
+  return (
+    <div key={product.name} className="card mt-5 p-3 col-4">
+      <img alt="" src="" className="card-img-top" />
+      <div className="card-body">
+        <h5 className="card-title">Name: {product.name}</h5>
+        <p className="card-text">Price: {product.price}€</p>
+        <p className="card-text">Id: {product.id}</p>
+        <button onClick={() => history.push('edit/' + product.id)} className="btn btn-primary ">Edit</button>
+        <button onClick={() => remove(product)} className="btn btn-danger deleted ml-3">Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
