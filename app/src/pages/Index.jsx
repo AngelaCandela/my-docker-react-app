f3c8aa69e6535ce747dea83c0d10654c35d9f213
Angela Candela <angelacandela@hotmail.it>
2021-08-27 11:24:16 +0200

    updated ProductsList (old Card) imports

--- app/src/pages/Index.jsx
+++ app/src/pages/Index.jsx
@@ -1,12 +1,12 @@
 import Header1 from '../components/atom/Header1'
 import Container from "../components/layout/Container"
-import Card from '../components/Card';
+import ProductsList from '../components/ProductsList';
 
 const Index = () => (
   <Container className="my-5">
     <Header1
         title={`Listado de productos ${process.env.REACT_APP_NAME}`}/>
-    <Card />
+    <ProductsList />
   </Container>
 );
 
