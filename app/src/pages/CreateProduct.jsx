15db241d3b0d76e5e7ca88a1541f3236957c4084
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-27 16:44:10 +0200

    Removo unnecessary prop from CreateProduct

--- app/src/pages/CreateProduct.jsx
+++ app/src/pages/CreateProduct.jsx
@@ -3,12 +3,12 @@ import Header1 from "../components/atom/Header1"
 import Container from "../components/layout/Container"
 import ProductForm from "../components/ProductForm";
 
-const CreateProduct = ({ setProducts }) => (
+const CreateProduct = () => (
     <Container className="my-5">
         <div className="row justify-content-md-center">
             <Header1 title="Create" />
         </div>
-        <ProductForm setProducts={setProducts} />
+        <ProductForm />
     </Container>
 );
 
