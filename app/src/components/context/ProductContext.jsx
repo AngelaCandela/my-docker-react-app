d308f5c2442441df896b7b842f1e6f93b6973fab
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-27 18:04:05 +0200

    add empty object to initialState of ProductContext

--- app/src/components/context/ProductContext.jsx
+++ app/src/components/context/ProductContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-export const ProductContext = createContext();
+export const ProductContext = createContext({});
 
 export const ProductProvider = ProductContext.Provider;
