706c8399b094f151380c88961f8f6389fcea4773
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-30 16:40:15 +0200

    add useState to usePageBottom

--- app/src/components/lib/usePageBottom.jsx
+++ app/src/components/lib/usePageBottom.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const usePageBottom = () => {
     const [bottom, setBottom] = useState(false);
 
     useEffect(() => {
-        const handleScroll = () =>{
+        const handleScroll = () => {
             const isBottom = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight;
             setBottom(isBottom);
         }
