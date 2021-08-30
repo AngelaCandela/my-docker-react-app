8bf86ca62915f1d9a2d97665fb1e0092d7e1618c
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-30 12:58:13 +0200

    create useCopyToClipbord component

---
+++ app/src/components/lib/useCopyToClipBoard.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import copy from "copy-text-to-clipboard";
+
+const useCopyToClipboard = () => {
+    const [isCopied, setCopied] = useState(false);
+
+    const handleCopy = (text) => {
+        if (typeof text === "string" || typeof text === "number") {
+            copy(texr.toString());
+            setCopied(true);
+        } else {
+            setCopied(false);
+            console.error(
+                `Cannot copy typeof ${typeof text} to clipboard, must be a string o number`
+            );
+        }
+    }
+    return [isCopied, handleCopy];
+}
+
+export default useCopyToClipboard;
