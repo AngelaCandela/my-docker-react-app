3555cc7e299bfc816b6555c02e56a302eb19e437
Damian Jose Chocron Martinez <z3n4.cs@gmail.com>
2021-08-30 14:29:38 +0200

    add state to bottom in usePageBottom hook component

--- app/src/components/lib/usePageBottom.jsx
+++ app/src/components/lib/usePageBottom.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const usePageBottom = () => {
+    const [bottom, setBottom] = useState(false);
+
     useEffect(() => {
         const isBottom = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight;
     }, []);
+
+    return bottom;
 }
 
 export default usePageBottom;
