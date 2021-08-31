367e34e87efaa0590ff71b43e3c4de7b754b2a99
Angela Candela <angelacandela@hotmail.it>
2021-08-31 14:44:55 +0200

    create Modal.js

---
+++ app/src/components/molecule/Modal.js
@@ -0,0 +1,20 @@
+import React, { useRef, useCallback } from 'react';
+
+const Modal = ({ text, isModalOpen, setModalOpen }) => {
+  const ref = useRef();
+
+  const closeModal = useCallback(
+    () => setModalOpen(false), []
+  );
+
+  return (
+    isModalOpen &&
+      <div className="modal">
+        <div ref={ref} className="modal-content">
+          <p className="text-center">{text || "👋 Hey, I'm a modal. Click anywhere outside of me to close."}</p>
+        </div>
+      </div>
+  );
+};
+
+export default Modal;
